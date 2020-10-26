be7953383c2abf1b784ea9dcafba89f69ef7c101
Ely Alamillo <ely0013@gmail.com>
2020-10-26 15:54:51 -0500

    feat(init-config): add cra and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-black min-h-screen flex items-center">
+      <div className="container mx-auto p-4 flex flex-wrap items-center">
+        <div className="w-full  text-center p-4">
+          <div className="text-6xl font-medium">CRA, Tailwind Starter</div>
+          <div className="text-xl font-medium mb-4">
+            <a href="https://reactjs.org/" className="underline text-blue-500 m-5">reactjs.org</a>
+            <a href="https://tailwindcss.com/" className="underline text-blue-500">tailwindcss.com</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
